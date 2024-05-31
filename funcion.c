1aebac2323e1c797b6026ed75ae7b4e3e366f6da
Andresss0 <168221342+Andresss0@users.noreply.github.com>
2024-05-31 13:10:36 -0500

    funcion.c

--- funcion.c
+++ funcion.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <string.h>
-#include "funcion.h" 
+#include "funcion.h"
  
 void ingresarProducto(Producto productos[], int *cantidadProductos) {
     if (*cantidadProductos >= MAX_PRODUCTOS) {
         printf("No se pueden ingresar más productos.\n");
         return;
     }
+ 
     printf("Ingrese el nombre del producto: ");
-    scanf("%s", productos[*cantidadProductos].nombre);
+    fflush(stdin); // Limpiar el buffer de entrada
+    fgets(productos[*cantidadProductos].nombre, sizeof(productos[*cantidadProductos].nombre), stdin);
+ 
     printf("Ingrese la cantidad: ");
     scanf("%d", &productos[*cantidadProductos].cantidad);
+    
     printf("Ingrese el precio: ");
     scanf("%f", &productos[*cantidadProductos].precio);
+ 
     (*cantidadProductos)++;
     printf("Producto agregado exitosamente.\n");
 }
@@ -20,13 +25,17 @@ void ingresarProducto(Producto productos[], int *cantidadProductos) {
 void editarProducto(Producto productos[], int cantidadProductos) {
     char nombre[50];
     printf("Ingrese el nombre del producto a editar: ");
-    scanf("%s", nombre);
+    fflush(stdin);
+    fgets(nombre, sizeof(nombre), stdin);
+    
     for (int i = 0; i < cantidadProductos; i++) {
         if (strcmp(productos[i].nombre, nombre) == 0) {
             printf("Ingrese la nueva cantidad: ");
             scanf("%d", &productos[i].cantidad);
+            
             printf("Ingrese el nuevo precio: ");
             scanf("%f", &productos[i].precio);
+            
             printf("Producto editado exitosamente.\n");
             return;
         }
@@ -37,7 +46,9 @@ void editarProducto(Producto productos[], int cantidadProductos) {
 void eliminarProducto(Producto productos[], int *cantidadProductos) {
     char nombre[50];
     printf("Ingrese el nombre del producto a eliminar: ");
-    scanf("%s", nombre);
+    fflush(stdin);
+    fgets(nombre, sizeof(nombre), stdin);
+ 
     for (int i = 0; i < *cantidadProductos; i++) {
         if (strcmp(productos[i].nombre, nombre) == 0) {
             for (int j = i; j < *cantidadProductos - 1; j++) {
